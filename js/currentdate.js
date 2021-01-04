adf47fc910e72e1bd2717b09eb5a80d9cccaa2fe
djjwengler <djjwengler@hotmail.com>
2021-01-04 17:56:41 -0500

    Added CSS, JS

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,4 @@
-const options = {weekday: 'long', day: 'numeric', month: "long", year:'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+const todaysdate = new Date();
+document.getElementById('currentdate').innerHTML = (todaysdate.getMonth() + 1) + "/" + todaysdate.getDate() + "/" + todaysdate.getFullYear() + " " + todaysdate.getHours() + ":" + todaysdate.getMinutes() + ":" + todaysdate.getSeconds();
+document.getElementById('currentyear').textContent = todaysdate.getFullYear();
