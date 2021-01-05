5040e1a1d0fada7bc1aef904ed52e93c3f546f01
djjwengler <djjwengler@hotmail.com>
2021-01-05 10:36:59 -0500

    fine tuning css/javascript

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,4 +1,20 @@
 
 const todaysdate = new Date();
-document.getElementById('currentdate').innerHTML = (todaysdate.getMonth() + 1) + "/" + todaysdate.getDate() + "/" + todaysdate.getFullYear() + " " + todaysdate.getHours() + ":" + todaysdate.getMinutes() + ":" + todaysdate.getSeconds();
+function zeroPad(n) {
+  if (n <= 9) {
+    n = "0" + n;
+  }
+  return n;
+}
+function correctMonth(m) {
+  return m+1;
+}
+  var hours = zeroPad(todaysdate.getHours());
+  var minutes = zeroPad(todaysdate.getMinutes());
+  var seconds = zeroPad(todaysdate.getSeconds());
+  var month = zeroPad(correctMonth(todaysdate.getMonth()));
+  var day = zeroPad(todaysdate.getDate());
+
+
+document.getElementById('currentdate').textContent = month + "/" + day + "/" + todaysdate.getFullYear() + " " + hours + ":" + minutes + ":" + seconds;
 document.getElementById('currentyear').textContent = todaysdate.getFullYear();
