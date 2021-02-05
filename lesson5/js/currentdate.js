d4372fdd161cbbea55512fc166b714671bf85ee4
djjwengler <djjwengler@hotmail.com>
2021-02-05 11:32:42 -0500

    Add forecast/grid summary

--- lesson5/js/currentdate.js
+++ lesson5/js/currentdate.js
@@ -4,3 +4,4 @@ const todaysdate = new Date();
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentdate').textContent = todaysdate.toLocaleDateString('en-GB', options);
 
+document.getElementById('currentyear').textContent = todaysdate.getFullYear();
