06842f063f3ac82400d13c61a220fe024124b8bb
djjwengler <djjwengler@hotmail.com>
2021-03-05 11:32:30 -0500

    delete js activities to help with pagespeed

--- lesson9/js/towndata.js
+++ lesson9/js/towndata.js
@@ -7,8 +7,8 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const townData = jsonObject['towns'];
-    const townArray = [5, 1, 6];
-    for (let i = 0; i < townArray.length; i++ ) {
+    for (let i = 0; i < townData.length; i++ ) {
+      if (townData[i].name == "Preston" || townData[i].name == "Fish Haven" || townData[i].name == "Soda Springs"){
       let town = document.createElement('section');
       let h2 = document.createElement('h2');
       let h3 = document.createElement('h3');
@@ -18,25 +18,17 @@ fetch(requestURL)
       let img = document.createElement('img');
       let act = document.createElement('section');
       let h4 = document.createElement('h4');
-      let ul = document.createElement('ul');
       
       
-      h2.textContent = townData[townArray[i]].name;
-      h3.textContent = townData[townArray[i]].motto;
-      h3_2.textContent = 'Year Founded: ' + townData[townArray[i]].yearFounded;
-      h3_3.textContent = 'Population: ' + townData[townArray[i]].currentPopulation;
-      h3_4.textContent = 'Annual Rain Fall: ' + townData[townArray[i]].averageRainfall;
-      img.setAttribute('src', townData[townArray[i]].photo);
-      img.setAttribute('alt', townData[townArray[i]].name + ' photo');
-      ul.setAttribute('class', townData[townArray[i]].name + 'act');
-      h4.textContent = townData[townArray[i]].name;
-      let eventArray = townData[townArray[i]].events
-      console.log(eventArray);
-      for (let j = 0; j<eventArray.length; j++){
-        let li = document.createElement('li');
-        li.textContent = eventArray[j];
-        ul.appendChild(li);
-      }
+      h2.textContent = townData[i].name;
+      h3.textContent = townData[i].motto;
+      h3_2.textContent = 'Year Founded: ' + townData[i].yearFounded;
+      h3_3.textContent = 'Population: ' + townData[i].currentPopulation;
+      h3_4.textContent = 'Annual Rain Fall: ' + townData[i].averageRainfall;
+      img.setAttribute('src', townData[i].photo);
+      img.setAttribute('alt', townData[i].name + ' photo');
+      img.setAttribute('loading', 'lazy');
+      
 
       town.appendChild(h2);
       town.appendChild(h3);
@@ -44,10 +36,9 @@ fetch(requestURL)
       town.appendChild(h3_3);
       town.appendChild(h3_4);
       town.appendChild(img);
-      act.appendChild(h4);
-      act.appendChild(ul);
+ 
 
       document.querySelector('div.town-info').appendChild(town);
-      document.querySelector('div.town-activities').appendChild(act);
-    }
+
+    }}
   });
