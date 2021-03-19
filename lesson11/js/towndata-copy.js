0c3f3a21aaac221741dfc936fcf0e96e06a3b472
djjwengler <djjwengler@hotmail.com>
2021-03-19 14:51:04 -0400

    change towndata to filter, help page speed

--- lesson11/js/towndata-copy.js
+++ lesson11/js/towndata-copy.js
@@ -6,9 +6,10 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
    // temporary checking for valid response and data parsing
+   
     const townData = jsonObject['towns'];
-    const townFilter = townData.filter(x => x.name == "Preston" || x.name =="Soda Springs" || x.name =="Fish Haven");
-    for (let i = 0; i < townFilter.length; i++ ) {
+    for (let i = 0; i < townData.length; i++ ) {
+      if (townData[i].name == "Preston" || townData[i].name == "Fish Haven" || townData[i].name == "Soda Springs"){
       let town = document.createElement('section');
       let h2 = document.createElement('h2');
       let h3 = document.createElement('h3');
@@ -17,14 +18,15 @@ fetch(requestURL)
       let h3_4 = document.createElement('h3');
       let img = document.createElement('img');
       
-      h2.textContent = townFilter[i].name;
-      h3.textContent = townFilter[i].motto;
-      h3_2.textContent = 'Year Founded: ' + townFilter[i].yearFounded;
-      h3_3.textContent = 'Population: ' + townFilter[i].currentPopulation;
-      h3_4.textContent = 'Annual Rain Fall: ' + townFilter[i].averageRainfall;
-      img.setAttribute('src', townFilter[i].photo);
-      img.setAttribute('alt', townFilter[i].name + ' photo');
+      h2.textContent = townData[i].name;
+      h3.textContent = townData[i].motto;
+      h3_2.textContent = 'Year Founded: ' + townData[i].yearFounded;
+      h3_3.textContent = 'Population: ' + townData[i].currentPopulation;
+      h3_4.textContent = 'Annual Rain Fall: ' + townData[i].averageRainfall;
+      img.setAttribute('src', townData[i].photo);
+      img.setAttribute('alt', townData[i].name + ' photo');
       img.setAttribute('loading', 'lazy');
+      
 
       town.appendChild(h2);
       town.appendChild(h3);
@@ -33,7 +35,8 @@ fetch(requestURL)
       town.appendChild(h3_4);
       town.appendChild(img);
  
+
       document.querySelector('div.town-info').appendChild(town);
 
-    }
+    }}
   });
